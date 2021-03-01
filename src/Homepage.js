59f9bae9bc0e03ab00731ea0fbd2c2c9aae52a8c
JoonsubHwang <j97194719@gmail.com>
2021-03-01 11:46:29 +0900

    Added links to News, Gallery, SocialMedia

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Homepage(props) {
     return (
-        <p>IZ*Feed</p>
+        <div>
+            <h1>IZ*Feed</h1>
+            <Link to='/news'> <p> News </p> </Link>
+            <Link to='/gallery'> <p> Gallery </p> </Link>
+            <Link to='/social-media'> <p> Social Media </p> </Link>
+        </div>
     );
 }
 
