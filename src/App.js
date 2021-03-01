6175d62d890c5943b22b70730d5425ebd8531a9e
JoonsubHwang <j97194719@gmail.com>
2021-03-01 11:38:01 +0900

    Added routes (Homepage, News, Gallery, SocialMedia

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './Homepage';
+import News from './News';
+import Gallery from './Gallery';
+import SocialMedia from './SocialMedia';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Homepage></Homepage>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => (
+        <Homepage />
+      )} />
+      <Route exact path='/news' render={() => (
+        <News />
+      )} />
+      <Route exact path='/gallery' render={() => (
+        <Gallery />
+      )} />
+      <Route exact path='/social-media' render={() => (
+        <SocialMedia />
+      )} />
+      <Route render={() => (
+        <h1>Not Found</h1>
+      )}/>
+    </Switch>
   );
 }
 
